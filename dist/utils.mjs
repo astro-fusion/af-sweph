8940aa435344d2e064bd5bea2e6d89a1e16de743
Bishal Ghimire <bishal.ghimire@gmail.com>
2025-12-28 21:39:52 +0545

    build(esm): release 0.1.8 with ESM shims

--- dist/utils.mjs
+++ dist/utils.mjs
@@ -1,3 +1,11 @@
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /**
  * Utility Functions for @AstroFusion/sweph
  */
